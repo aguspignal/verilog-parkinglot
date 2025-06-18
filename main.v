ccb05cab10facedb041f351bc405c0a3dcfb10b1
aguspignal <agustinperezpignalberi@gmail.com>
2025-06-18 17:20:28 -0300

    fsm

--- main.v
+++ main.v
@@ -5,7 +5,7 @@ module main (
     output [2:0] count
 );
 
-wire fsm_in_output, fsm_out_output;
+wire fsm_in_output, fsm_out_output, fsm_output;
 
 fsm_in fsm_entrada_vehiculo (
     .clk(clk),
@@ -22,6 +22,14 @@ fsm_out fsm_salida_vehiculo (
     .reset(reset | (count == 3'b000)),
     .y(fsm_out_output)
 );
+
+// fsm fsm_ (
+//     .clk(clk)
+//     .a(a),
+//     .b(b),
+//     .reset(reset | (count == 3'b111)),
+//     .y(fsm_output)
+// )
 
 cont_ascdesc_3b contador (
     .clk(fsm_in_output | fsm_out_output | reset),
